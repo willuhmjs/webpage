c8e411e8e0a52fb8d22a8726fba6057fc3bc4e35
William Faircloth <willuhmjs@gmail.com>
2023-03-19 22:01:18 +0000

    chore: move fontawesome import to app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,14 @@
 	import Repos from '../lib/Repos.svelte';
 </script>
 
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+	integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+	crossorigin="anonymous"
+	referrerpolicy="no-referrer"
+/>
+
 <div class="wrapper">
 	<Header />
 	<Repos />
