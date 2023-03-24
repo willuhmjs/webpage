98835e37998065ca1002cb05d9468c0304f1432f
willuhmjs <willuhmjs@gmail.com>
2023-03-24 12:13:14 -0400

    style: format

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,93 +1,96 @@
-<!-- large pfp icon on the left, info about me on the right. include header and description. maybe get it dynamically from api-->
 <!-- use icons for stuff like location, mimic GitHub -->
 <script lang="ts">
-    import { onMount } from 'svelte';
-    let user;
-    async function updateUser() {
-        const data = await fetch('https://api.github.com/users/willuhmjs?');
-        user = await data.json();
-    }
-    onMount(updateUser);
+	import { onMount } from 'svelte';
+	let user;
+	async function updateUser() {
+		const data = await fetch('https://api.github.com/users/willuhmjs?');
+		user = await data.json();
+	}
+	onMount(updateUser);
 
-var offset = new Date().getTimezoneOffset();// getting offset to make time in gmt+0 zone (UTC) (for gmt+5 offset comes as -300 minutes)
-let date = new Date();
-date.setMinutes ( date.getMinutes() + offset);// date now in UTC time
-            
-var easternTimeOffset = -240; //for dayLight saving, Eastern time become 4 hours behind UTC thats why its offset is -4x60 = -240 minutes. So when Day light is not active the offset will be -300
-date.setMinutes ( date.getMinutes() + easternTimeOffset);
+	var offset = new Date().getTimezoneOffset(); // getting offset to make time in gmt+0 zone (UTC) (for gmt+5 offset comes as -300 minutes)
+	let date = new Date();
+	date.setMinutes(date.getMinutes() + offset); // date now in UTC time
 
+	var easternTimeOffset = -240; //for dayLight saving, Eastern time become 4 hours behind UTC thats why its offset is -4x60 = -240 minutes. So when Day light is not active the offset will be -300
+	date.setMinutes(date.getMinutes() + easternTimeOffset);
 </script>
-{#if user}
-    <div class="wrapper">
-        <div>
-            <h1>{user.name}</h1>
-            <p>{user.bio}</p>
-        <div class="links">
-            <a href="https://www.bible.com/bible/111/rev.21.2">
-                <i class="fa-solid fa-map-marker-alt"></i>
-                {user.location}
-            </a>
-            <a href="mailto:me@willuhmjs.com?subject=Hello%20there!">
-                <i class="fa-solid fa-envelope"></i>
-                me@willuhmjs.com
-            </a>
-            <a href="https://time.is/New_York">
-                <i class="fa-solid fa-clock"></i>
-                {date.getHours().toString().padStart(2, '0')}:{date.getMinutes().toString().padStart(2, '0')}
-            </a>
-        </div>
-        </div>
-        <img src={user.avatar_url} alt="My profile icon" />
 
-    </div>
+<!-- large pfp icon on the left, info about me on the right. include header and description. maybe get it dynamically from api-->
+
+{#if user}
+	<div class="wrapper">
+		<div>
+			<h1>{user.name}</h1>
+			<p>{user.bio}</p>
+			<div class="links">
+				<a href="https://www.bible.com/bible/111/rev.21.2">
+					<i class="fa-solid fa-map-marker-alt" />
+					{user.location}
+				</a>
+				<a href="mailto:me@willuhmjs.com?subject=Hello%20there!">
+					<i class="fa-solid fa-envelope" />
+					me@willuhmjs.com
+				</a>
+				<a href="https://time.is/New_York">
+					<i class="fa-solid fa-clock" />
+					{date.getHours().toString().padStart(2, '0')}:{date
+						.getMinutes()
+						.toString()
+						.padStart(2, '0')}
+				</a>
+			</div>
+		</div>
+		<img src={user.avatar_url} alt="My profile icon" />
+	</div>
 {/if}
 
 <style>
-    .wrapper {
-        display: flex;
-        box-sizing: border-box;
-        margin: 2rem auto;
-        max-width: 600px;
-    }
+	.wrapper {
+		display: flex;
+		box-sizing: border-box;
+		margin: 2rem auto;
+		max-width: 600px;
+	}
 
-    .wrapper img {
-        border-radius: 50%;
-        max-width: 15rem;
-    }
+	.wrapper img {
+		border-radius: 50%;
+		max-width: 15rem;
+	}
 
-    .wrapper div {
-        margin-right: auto;
+	.wrapper div {
+		margin-right: auto;
+	}
 
-    }
+	.links {
+		display: flex;
+		flex-direction: column;
+	}
 
-    .links {
-        display: flex; flex-direction: column;
-    }
+	.links a {
+		margin: 5px 0;
+		text-decoration: none;
+		color: inherit;
+	}
 
-    .links a {
-        margin: 5px 0;
-        text-decoration: none;
-        color: inherit;
-    }
+	.links a i {
+		margin-right: 6px;
+	}
 
-    .links a i {
-        margin-right: 6px;
-    }
+	.fa-map-marker-alt {
+		color: #ff4747;
+	}
 
-    .fa-map-marker-alt {
-        color: #ff4747;
-    }
+	.fa-envelope {
+		color: #50ff47;
+	}
 
-    .fa-envelope {
-        color: #50ff47;
-    }
+	.fa-clock {
+		color: #d747ff;
+	}
 
-    .fa-clock {
-        color: #d747ff;
-    }
-
-    a:hover {
-        filter: brightness(0.5);
-        color: #333;
-    }
+	a:hover {
+		filter: brightness(0.5);
+		color: #333;
+	}
 </style>
