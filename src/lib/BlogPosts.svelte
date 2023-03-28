06492076fff754d779547104317d85ecc89a6d70
willuhmjs <willuhmjs@gmail.com>
2023-03-27 21:30:27 -0400

    feat: relative paths ftw

--- src/lib/BlogPosts.svelte
+++ src/lib/BlogPosts.svelte
@@ -1,24 +1,24 @@
 <div class="wrapper">
 	<div class="blogPost">
-		<a href="/blog/post"><h2>cloudflare tunnels + raspberry pi</h2></a>
+		<a href="./blog/post"><h2>cloudflare tunnels + raspberry pi</h2></a>
 		<div class="blogPostDescription">
 			<p>access your raspberry pi from anywhere in the world with cloudflare tunnels</p>
 		</div>
 	</div>
 	<div class="blogPost">
-		<a href="/blog/post"><h2>raspberry pi cooling techniques</h2></a>
+		<a href="./blog/post"><h2>raspberry pi cooling techniques</h2></a>
 		<div class="blogPostDescription">
 			<p>how to make your raspberry pi as cool as a cucumber, or a raspberry</p>
 		</div>
 	</div>
     <div class="blogPost">
-		<a href="/blog/post"><h2>wtf is additive manufacturing?</h2></a>
+		<a href="./blog/post"><h2>wtf is additive manufacturing?</h2></a>
 		<div class="blogPostDescription">
 			<p>3d printing, or additive manufacturing, is quite the process!</p>
 		</div>
 	</div>
 	<div class="blogPost">
-		<a href="/blog/post"><h2>there will be stuff here &lt;3</h2></a>
+		<a href="./blog/post"><h2>there will be stuff here &lt;3</h2></a>
 		<div class="blogPostDescription">
 			<p>there will totally be stuff here soon, i promise</p>
 		</div>
