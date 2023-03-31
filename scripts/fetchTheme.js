6900930a85a9a908041ea6a4a08e83c352b933f6
William Faircloth <willuhmjs@gmail.com>
2023-03-31 17:28:08 +0000

    chore: remove comment

--- scripts/fetchTheme.js
+++ scripts/fetchTheme.js
@@ -11,11 +11,8 @@ async function main() {
 	const dark = await fetchTheme('github-dark');
 	const light = await fetchTheme('github-light');
 
-	// write to src/themes/dark.json and light.json using fs.writeFileSync
 	fs.writeFileSync('themes/light.json', JSON.stringify(light));
 	fs.writeFileSync('themes/dark.json', JSON.stringify(dark));
-
-	//fs.writeFileSync(resolve("themes/dark.json"), JSON.stringify(dark));
 }
 
 await main();
