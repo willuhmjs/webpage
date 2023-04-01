a7658f6f1747da7ced021e6731c1f2c1de3550d0
William Faircloth <willuhmjs@gmail.com>
2023-04-01 18:43:12 -0400

    fix: position at bottom

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -18,10 +18,7 @@
 		justify-content: center;
 		flex-wrap: wrap;
 		padding: 1ch;
-        position: fixed;
-        left: 0;
-        right: 0;
-        bottom: 0;
+        padding-bottom: 0;
 	}
 
 	i {
