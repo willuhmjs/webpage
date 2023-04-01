272ac834d8b9a6d53564da70a1a425aede487c7f
William Faircloth <willuhmjs@gmail.com>
2023-04-01 18:30:31 -0400

    feat: header/footer test cases for main pages

--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,20 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected header and navbar', async ({ page }) => {
-	await page.goto('/');
-	const navbar = page.getByRole('navigation');
-	const header = page.getByRole('banner');
-	await expect(header).toBeVisible();
-	await expect(navbar).toBeVisible();
-	await expect(navbar).toHaveText('willuhmjs   blog  about');
+const pages = ['/', '/about'];
+test('each main page has expected header and footer', async ({ page }) => {
+	for (const pagePath of pages) {
+		await page.goto(pagePath);
+
+		// header
+		const header = page.getByRole('banner');
+		const navbar = page.getByRole('navigation');
+		await expect(header).toBeVisible();
+		await expect(navbar).toBeVisible();
+		await expect(navbar).toHaveText('willuhmjs   blog  about');
+
+		// footer
+		const footer = page.getByRole('contentinfo');
+		await expect(footer).toBeVisible();
+		await expect(footer).toHaveText('fix typo willuhmjs');
+	}
 });
