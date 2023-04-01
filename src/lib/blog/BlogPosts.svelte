bfd5e4b2be7aa5fc8de68edfa8c53be94d318ed7
William Faircloth <willuhmjs@gmail.com>
2023-04-01 18:17:26 -0400

    refactor: use eager

--- src/lib/blog/BlogPosts.svelte
+++ src/lib/blog/BlogPosts.svelte
@@ -1,29 +1,23 @@
 <script>
-	const raw = import.meta.glob('../../routes/article/**/*.md');
+	const raw = import.meta.glob('../../routes/article/**/*.md', { eager: true });
 </script>
 
 <section>
 	<ul>
-		{#each Object.entries(raw) as [_, resolve] (resolve)}
-			{#await resolve()}
-				<p>Loading Posts...</p>
-			{:then data}
-				<li>
-					<h2><a href="/article/{data.metadata.slug}">{data.metadata.title}</a></h2>
-					<p class="subtitle">
-						<time datetime={data.metadata.datetime}
-							>{new Date(data.metadata.date).toLocaleDateString('en-US', {
-								month: 'short',
-								day: 'numeric',
-								year: 'numeric',
-								timeZone: 'UTC'
-							})}</time
-						>
-					</p>
-				</li>
-			{:catch error}
-				<p>{error.message}</p>
-			{/await}
+		{#each Object.entries(raw) as [_, data] (data)}
+			<li>
+				<h2><a href="/article/{data.metadata.slug}">{data.metadata.title}</a></h2>
+				<p class="subtitle">
+					<time datetime={data.metadata.datetime}
+						>{new Date(data.metadata.date).toLocaleDateString('en-US', {
+							month: 'short',
+							day: 'numeric',
+							year: 'numeric',
+							timeZone: 'UTC'
+						})}</time
+					>
+				</p>
+			</li>
 		{/each}
 	</ul>
 </section>
