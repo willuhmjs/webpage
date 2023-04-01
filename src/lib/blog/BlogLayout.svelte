8d3261021e343ab740ac4f7c51e9eb2660526bee
willuhmjs <willuhmjs@gmail.com>
2023-04-01 14:37:27 -0400

    feat: post data and meta path

--- src/lib/blog/BlogLayout.svelte
+++ src/lib/blog/BlogLayout.svelte
@@ -1,1 +1,22 @@
+<script>
+    import { filepath } from "$lib/filepath";
+
+    /** @type {string} */
+    export let title;
+
+    /** @type {string} */
+    export let description;
+
+    /** @type {string} */
+    export let slug;
+    
+    $filepath = `/src/routes/article/${slug}/+page.md`;
+</script>
+
+<svelte:head>
+    <title>{title} - willuhmjs.com</title>
+    <meta name="description" content={description} />
+</svelte:head>
+
+<h1>{title}</h1>
 <slot />
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Card from '$lib/about/Card.svelte';
 	import Bio from '$lib/about/Bio.svelte';
+	import { filepath } from '$lib/filepath';
+	$filepath = '/src/routes/about/+page.svelte';
 </script>
 
 <Card />
