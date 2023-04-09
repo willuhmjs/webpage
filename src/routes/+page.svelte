210305d6809a9bcaec0ddc01f49082fedbaca48e
willuhmjs <willuhmjs@gmail.com>
2023-04-09 19:32:29 -0400

    feat: image meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,13 +15,13 @@
 	<meta property="og:title" content="willuhmjs.com" />
 	<meta property="og:description" content="A software development blog by @willuhmjs." />
 	<meta property="og:url" content="https://www.willuhmjs.com/" />
-	<!--<meta property="og:image" content="" />-->
+	<meta property="og:image" content="https://willuhmjs.com/pfp.png" />
 
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="willuhmjs.com" />
 	<meta name="twitter:description" content="A software development blog by @willuhmjs." />
-	<!--<meta name="twitter:image" content="https://www.myblog.com/images/twitter-image.jpg" />-->
+	<meta name="twitter:image" content="https://willuhmjs.com/pfp.png" />
 	<meta name="twitter:site" content="@willuhmjs" />
 	<meta name="twitter:creator" content="@willuhmjs" />
 </svelte:head>
