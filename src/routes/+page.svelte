599a6e49437a076fb9fdc3792a6da06a664fed8d
willuhmjs <willuhmjs@gmail.com>
2023-04-09 18:28:45 -0400

    feat: more meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,30 @@
 	import { filepath } from '$lib/filepath';
 	$filepath = '/src/routes/+page.svelte';
 </script>
+<svelte:head>
+	<title>Posts - willuhmjs.com</title>
+	<meta name="description" content="A software development blog by @willuhmjs." />
+	<meta name="keywords" content="blog, articles, technology, coding, programming, developer" />
+	<meta name="author" content="William Faircloth" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="theme-color" content="#08608c" />
+	<meta name="robots" content="index, follow" />
+	<link rel="canonical" href="https://www.willuhmjs.com/" />
+
+	<!-- Open Graph Meta Tags -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Posts - willuhmjs.com" />
+	<meta property="og:description" content="A software development blog by @willuhmjs." />
+	<meta property="og:url" content="https://www.willuhmjs.com/" />
+	<!--<meta property="og:image" content="" />-->
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Posts - willuhmjs.com" />
+	<meta name="twitter:description" content="A software development blog by @willuhmjs." />
+	<!--<meta name="twitter:image" content="https://www.myblog.com/images/twitter-image.jpg" />-->
+	<meta name="twitter:site" content="@willuhmjs" />
+  	<meta name="twitter:creator" content="@willuhmjs" />
+</svelte:head>
 
 <BlogPosts />
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,30 @@
 	import { filepath } from '$lib/filepath';
 	$filepath = '/src/routes/about/+page.svelte';
 </script>
+<svelte:head>
+	<title>About - willuhmjs.com</title>
+	<meta name="description" content="Some information about me, @willuhmjs." />
+	<meta name="keywords" content="blog, articles, technology, coding, programming, developer" />
+	<meta name="author" content="William Faircloth" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="theme-color" content="#08608c" />
+	<meta name="robots" content="index, follow" />
+	<link rel="canonical" href="https://www.willuhmjs.com/" />
 
+	<!-- Open Graph Meta Tags -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="About - willuhmjs.com" />
+	<meta property="og:description" content="Some information about me, @willuhmjs." />
+	<meta property="og:url" content="https://www.willuhmjs.com/" />
+	<!--<meta property="og:image" content="" />-->
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="About - willuhmjs.com" />
+	<meta name="twitter:description" content="Some information about me, @willuhmjs." />
+	<!--<meta name="twitter:image" content="https://www.myblog.com/images/twitter-image.jpg" />-->
+	<meta name="twitter:site" content="@willuhmjs" />
+  	<meta name="twitter:creator" content="@willuhmjs" />
+</svelte:head>
 <Card />
 <Bio />
