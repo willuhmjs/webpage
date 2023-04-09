48cf980230936fdb5339ba689e373d24b3c4ba8d
willuhmjs <willuhmjs@gmail.com>
2023-04-09 19:17:59 -0400

    feat: publisher meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="theme-color" content="#08608c" />
 	<meta name="robots" content="index, follow" />
+	<meta name="publisher" content="William Faircloth">
 </svelte:head>
 
 <Header />
